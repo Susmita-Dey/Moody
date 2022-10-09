17b73c218e4d4593f1fd53e33ee5ed37eae2abf7
Shyam3050 <nimmanashyam3050@gmail.com>
2022-10-09 12:29:58 +0530

    fetching animation and search icon added

--- js/recipe.js
+++ js/recipe.js
@@ -26,10 +26,11 @@ async function generateRecipe() {
 }
 */
 
-const form = document.querySelector('form');
-const searchResult = document.querySelector('.search')
-const container = document.querySelector('.container');
-let userQuery = '';
+const loader = document.getElementById("loader");
+const form = document.querySelector("form");
+const searchResult = document.querySelector(".search");
+const container = document.querySelector(".container");
+let userQuery = "";
 
 // var i = 0;
 // var txt = 'Live, love, eat...';
@@ -47,37 +48,38 @@ let userQuery = '';
 const ID = "491c0a25";
 const key = "90f6192735aad01f8dc20fb18ee53869";
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    userQuery = e.target.querySelector('input').value;
-    console.log(userQuery);
-    fetchData();
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchResult.innerHTML = ""
+  userQuery = e.target.querySelector("input").value;
+  console.log(userQuery);
+   if(!userQuery) return;
+  fetchData();
+});
 
 async function fetchData() {
-    const baseURL = `https://api.edamam.com/search?q=${userQuery}&app_id=${ID}&app_key=${key}`;
-    const response = await fetch(baseURL);
-    const data = await response.json();
-    createContent(data.hits);
-    console.log(data);
+  loader.classList.add("loader")
+  const baseURL = `https://api.edamam.com/search?q=${userQuery}&app_id=${ID}&app_key=${key}`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  console.log(data.hits);
+  createContent(data.hits);
+  loader.classList.remove("loader")
 }
 
 function createContent(results) {
-    let initialContent = '';
-    results.map(result => {
-        initialContent +=
-
-            `<div class="item">
+  let initialContent = "";
+  results.map((result) => {
+    initialContent += `<div class="item">
         <img src="${result.recipe.image}" alt="">
         <div class="flex-container">
             <h1 class='title'>${result.recipe.label}</h1>
             <a class='view-btn' href='${result.recipe.url}'>View Recipe</a>
         </div>
-        <p class='recipe-desc'>Calories : ${result.recipe.calories.toFixed(2)}</p>
-    </div>`
-
-
-    })
-
-    searchResult.innerHTML = initialContent;
+        <p class='recipe-desc'>Calories : ${result.recipe.calories.toFixed(
+          2
+        )}</p>
+    </div>`;
+  });
+  searchResult.innerHTML = initialContent;
 }
